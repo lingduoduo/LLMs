e46072f88b76f945fc1ce2830b6c91bd4c95ef32
Ling <linghypshen@gmail.com>
2025-06-11 17:30:31 -0400

    Update Redis_sampled.py

--- notebook/Redis_sampled.py
+++ notebook/Redis_sampled.py
@@ -1,14 +1,24 @@
 from redis.commands.search.suggestion import Suggestion
 
+total = 0
+BATCH_SIZE = 10_000
+
 with redis_client.pipeline(transaction=False) as pipe:
+    batch_count = 0
     for key, val in most_common_keywords.items():
-        total  = 0
         pipe.ft().sugadd(
             'top_action_keywords',
-            Suggestion(key, val),
+            Suggestion(key, float(val)),
             increment=True,
         )
         total += 1
-    pipe.execute()
+        batch_count += 1
+
+        if batch_count >= BATCH_SIZE:
+            pipe.execute()
+            batch_count = 0
+
+    if batch_count > 0:
+        pipe.execute()
 
 print(f"Inserted/updated {total:,} keywords into 'top_action_keywords'")
